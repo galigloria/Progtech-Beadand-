354978b0359d71cd6968c164486b32c1414ccacd
galigloria <85185233+galigloria@users.noreply.github.com>
2024-05-01 10:50:08 +0200

    Adatbázis
    A felhasználói adatok adattáblájénak létrehozása és a táblán a különböző lekérdezések végrehajtása.

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿Create Table Users(
+id INT PRIMARY KEY IDENTITY(1,1),
+username VARCHAR(MAX) NULL, 
+password VARCHAR(MAX) NULL, 
+profile_image VARCHAR(MAX) NULL, 
+role VARCHAR(MAX) NULL, 
+status VARCHAR(MAX) NULL, 
+reg_date DATE NULL
+)
+SELECT*FROM Users
+INSERT INTO Users(username,password,profile_image,role,status,reg_date) VALUES ('Glória','gria33','','Admin','Active','2024-04-05')
