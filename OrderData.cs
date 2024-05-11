f2a92eb14657e262b98642c43cb23ccb6975b363
galigloria <85185233+galigloria@users.noreply.github.com>
2024-05-11 20:26:34 +0200

    Add files via upload
    A rendeléshez szükséges adattáblák osztályainak kialakítása

---
+++ OrderData.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LegoStoreManagementSystem
+{
+    public class OrderData : ICloneable
+    {
+        public int CostumerID { get; set; } 
+        public string LegoID { get; set; } //1
+        public string LegoName { get; set; } //2
+        public string LegoType { get; set; } //3
+        public float LegoPrice { get; set; } //4
+        public int LegoQuantity { get; set; } //5
+        private SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Glóri\Documents\lego.mdf;Integrated Security=True;Connect Timeout=30;TrustServerCertificate=False");
+
+        public List<OrderData> orderListData()
+        {
+            List<OrderData> legoOrderData = new List<OrderData>();
+            if(connect.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    connect.Open();
+                    int customerID = 0;
+                    string selectCuctomerData = "SELECT MAX(customer_ID) * FROM ORDERS";
+                    using(SqlCommand getCustomerData = new SqlCommand(selectCuctomerData,connect))
+                    {
+                        object result = getCustomerData.ExecuteScalar();
+                        if(result != DBNull.Value)
+                        {
+                            int temp = Convert.ToInt32(result);
+                            //if (temp == 0)
+                            //{
+                            //    customerID = 1;
+                            //}
+                            //else
+                            //{
+                            //    customerID = temp;
+                            //}
+                        }
+                        else
+                        {
+                            Console.WriteLine("Hibás azonosító!");
+                        }
+                    }
+                    string selectOrders = "SELECT * FROM ORDERS WHERE customer_ID = @customerID";
+                    using(SqlCommand cmd = new SqlCommand(selectOrders, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@customerID", customerID);
+                        SqlDataReader dataReader= cmd.ExecuteReader();
+                        while (dataReader.Read())
+                        {
+                            OrderData orderData = new OrderData();
+                            orderData.CostumerID = (int)dataReader["customer_ID"];
+                            orderData.LegoID = dataReader["lego_id"].ToString();
+                            orderData.LegoName = dataReader["lego_name"].ToString();
+                            orderData.LegoType = dataReader["lego_type"].ToString();
+                            orderData.LegoPrice = (float)dataReader["lego_price"];
+                            orderData.LegoQuantity = (int)dataReader["lego_quantity"];
+                            legoOrderData.Add(orderData);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Hibás csatlakozás:" +ex);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+                
+            }
+            return legoOrderData;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ OrderLegoData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LegoStoreManagementSystem
+{
+    public class OrderLegoData : Lego
+    {
+        //public int ID { get; set; }
+        //public string LegoID { get; set; } //1
+        //public string LegoName { get; set; } //2
+        //public string LegoType { get; set; } //3
+        //public string LegoStock { get; set; } //4
+        //public string LegoPrice { get; set; } //5
+        //public string LegoStatus { get; set; } //6
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Glóri\Documents\lego.mdf;Integrated Security=True;Connect Timeout=30;TrustServerCertificate=False");
+
+        public override int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string LegoID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string LegoName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string LegoType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string LegoStock { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string LegoPrice { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string LegoStatus { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string LegoImage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string DateInsert { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string DateUpdate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public List<OrderLegoData> availableLegoData()
+        {
+            List<OrderLegoData> listLego = new List<OrderLegoData>();
+            if (connect.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    connect.Open();
+                    string selectLego = "SELECT * FROM LEGO WHERE lego_status = @legoStatus AND date_delete IS NULL";
+                    using (SqlCommand cmd = new SqlCommand(selectLego, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@legoSatus", "Jelenleg elérhető");
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            OrderLegoData adp = new OrderLegoData();
+                            adp.ID = (int)reader["id"];
+                            adp.LegoName = reader["lego_name"].ToString();
+                            adp.LegoType = reader["lego_type"].ToString();
+                            adp.LegoStock = reader["lego_stock"].ToString();
+                            adp.LegoPrice = reader["lego_price"].ToString();
+                            adp.LegoStatus = reader["lego_status"].ToString();
+
+                            listLego.Add(adp);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Sikertelen csatlakozás:" + ex);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+            return listLego;
+        }
+
+    }
+}
+
+
