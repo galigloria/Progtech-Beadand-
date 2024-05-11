db4c058fbc462e070807a7f246484d1b4711c274
galigloria <85185233+galigloria@users.noreply.github.com>
2024-05-11 20:29:23 +0200

    Add files via upload
    A megjelenítendő adattáblákat külön osztályokba szerveztem

---
+++ ProductsDataGridView.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LegoStoreManagementSystem
+{
+    public class ProductsDataGridView : DataGridView
+    {
+        public ProductsDataGridView()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            this.AllowUserToAddRows = false;
+            this.AllowUserToDeleteRows = false;
+            this.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Name = "DataGridView1";
+            this.ReadOnly = true;
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        private void InitializeDataGridView()
+        {
+            DataGridView dataGridView = new DataGridView();
+            dataGridView.Dock = DockStyle.Fill;
+            Controls.Add(dataGridView);
+
+            // DataGridView oszlopainak beállítása
+            dataGridView.AutoGenerateColumns = false;
+
+            // ID oszlop
+            DataGridViewTextBoxColumn idColumn = new DataGridViewTextBoxColumn();
+            idColumn.DataPropertyName = "id";
+            idColumn.HeaderText = "ID";
+            idColumn.Name = "id";
+            dataGridView.Columns.Add(idColumn);
+
+            // LegoID oszlop
+            DataGridViewTextBoxColumn legoIDColumn = new DataGridViewTextBoxColumn();
+            legoIDColumn.DataPropertyName = "lego_id";
+            legoIDColumn.HeaderText = "Lego Azonosítója";
+            legoIDColumn.Name = "lego_id";
+            dataGridView.Columns.Add(legoIDColumn);
+
+            // Name oszlop
+            DataGridViewTextBoxColumn nameColumn = new DataGridViewTextBoxColumn();
+            nameColumn.DataPropertyName = "lego_name";
+            nameColumn.HeaderText = "Lego neve";
+            nameColumn.Name = "lego_name";
+            dataGridView.Columns.Add(nameColumn);
+
+            // LegoType oszlop
+            DataGridViewTextBoxColumn legoTypeColumn = new DataGridViewTextBoxColumn();
+            legoTypeColumn.DataPropertyName = "lego_type";
+            legoTypeColumn.HeaderText = "Leg Típusa";
+            legoTypeColumn.Name = "lego_type";
+            dataGridView.Columns.Add(legoTypeColumn);
+
+            // LegoStock oszlop
+            DataGridViewTextBoxColumn legoStockColumn = new DataGridViewTextBoxColumn();
+            legoStockColumn.DataPropertyName = "lego_stock";
+            legoStockColumn.HeaderText = "Készleten lévő mennyiség";
+            legoStockColumn.Name = "lego_stock";
+            dataGridView.Columns.Add(legoStockColumn);
+
+            // LegoPrice oszlop
+            DataGridViewTextBoxColumn legoPriceColumn = new DataGridViewTextBoxColumn();
+            legoPriceColumn.DataPropertyName = "lego_price";
+            legoPriceColumn.HeaderText = "Leg ára";
+            legoPriceColumn.Name = "lego_price";
+            dataGridView.Columns.Add(legoPriceColumn);
+
+            // LegoStatus oszlop
+            DataGridViewTextBoxColumn legoStatusColumn = new DataGridViewTextBoxColumn();
+            legoStatusColumn.DataPropertyName = "lego_status";
+            legoStatusColumn.HeaderText = "Lego státusza";
+            legoStatusColumn.Name = "lego_status";
+            dataGridView.Columns.Add(legoStatusColumn);
+
+            // LegoImage oszlop
+            DataGridViewTextBoxColumn legoImageColumn = new DataGridViewTextBoxColumn();
+            legoImageColumn.DataPropertyName = "lego_image";
+            legoImageColumn.HeaderText = "Betöltött kép";
+            legoImageColumn.Name = "lego_image";
+            dataGridView.Columns.Add(legoImageColumn);
+
+            // DateInsert oszlop
+            DataGridViewTextBoxColumn dateInsertColumn = new DataGridViewTextBoxColumn();
+            dateInsertColumn.DataPropertyName = "date_insert";
+            dateInsertColumn.HeaderText = "Beszúrás időpontja";
+            dateInsertColumn.Name = "date_insert";
+            dataGridView.Columns.Add(dateInsertColumn);
+
+            // DateUpdate oszlop
+            DataGridViewTextBoxColumn dateUpdateColumn = new DataGridViewTextBoxColumn();
+            dateUpdateColumn.DataPropertyName = "date_update";
+            dateUpdateColumn.HeaderText = "Módosítás időpontja";
+            dateUpdateColumn.Name = "date_update";
+            dataGridView.Columns.Add(dateUpdateColumn);
+
+            // DateDelete oszlop
+            DataGridViewTextBoxColumn dateDeleteColumn = new DataGridViewTextBoxColumn();
+            dateDeleteColumn.DataPropertyName = "date_delete";
+            dateDeleteColumn.HeaderText = "Törlés időpontja";
+            dateDeleteColumn.Name = "date_delete";
+            dataGridView.Columns.Add(dateDeleteColumn);
+            LegoData legoData = new LegoData();
+            dataGridView.DataSource = legoData.LegoDataList();
+
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView.MultiSelect = false;
+        }
+    }
+}
---
+++ UserDataGridView.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LegoStoreManagementSystem
+{
+    public class UserDataGridView: DataGridView
+    {
+        public UserDataGridView()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            this.AllowUserToAddRows = false;
+            this.AllowUserToDeleteRows = false;
+            this.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Name = "DataGridView1";
+            this.ReadOnly = true;
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        private void InitializeDataGridView()
+        {
+            DataGridView dataGridView = new DataGridView();
+            dataGridView.Dock = DockStyle.Fill;
+            Controls.Add(dataGridView);
+
+            // DataGridView oszlopainak beállítása
+            dataGridView.AutoGenerateColumns = false;
+
+            // ID oszlop
+            DataGridViewTextBoxColumn idColumn = new DataGridViewTextBoxColumn();
+            idColumn.DataPropertyName = "id";
+            idColumn.HeaderText = "ID";
+            idColumn.Name = "id";
+            dataGridView.Columns.Add(idColumn);
+
+           
+            // Name oszlop
+            DataGridViewTextBoxColumn nameColumn = new DataGridViewTextBoxColumn();
+            nameColumn.DataPropertyName = "username";
+            nameColumn.HeaderText = "Felhasználónév";
+            nameColumn.Name = "username";
+            dataGridView.Columns.Add(nameColumn);
+
+            // LegoType oszlop
+            DataGridViewTextBoxColumn passwordColumn = new DataGridViewTextBoxColumn();
+           passwordColumn.DataPropertyName = "password";
+            passwordColumn.HeaderText = "Jelszó";
+            passwordColumn.Name = "password";
+            dataGridView.Columns.Add(passwordColumn);
+
+            // LegoStock oszlop
+            DataGridViewTextBoxColumn profImageColumn = new DataGridViewTextBoxColumn();
+            profImageColumn.DataPropertyName = "prof_image";
+            profImageColumn.HeaderText = "Profilkép";
+            profImageColumn.Name = "pro_image";
+            dataGridView.Columns.Add(profImageColumn);
+
+            // LegoPrice oszlop
+            DataGridViewTextBoxColumn roleColumn = new DataGridViewTextBoxColumn();
+            roleColumn.DataPropertyName = "role";
+            roleColumn.HeaderText = "Munkakör";
+            roleColumn.Name = "role";
+            dataGridView.Columns.Add(roleColumn);
+
+            // LegoStatus oszlop
+            DataGridViewTextBoxColumn userStatusColumn = new DataGridViewTextBoxColumn();
+            userStatusColumn.DataPropertyName = "status";
+            userStatusColumn.HeaderText = "Státusz";
+            userStatusColumn.Name = "status";
+            dataGridView.Columns.Add(userStatusColumn);
+
+            // LegoImage oszlop
+            DataGridViewTextBoxColumn regDateColumn = new DataGridViewTextBoxColumn();
+            regDateColumn.DataPropertyName = "reg_date";
+            regDateColumn.HeaderText = "Regisztárlás időpontja";
+            regDateColumn.Name = "reg_date";
+            dataGridView.Columns.Add(regDateColumn);
+
+            
+           
+            UserData userData = new UserData();
+            dataGridView.DataSource = userData.usersDatas();
+
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView.MultiSelect = false;
+        }
+    }
+}
