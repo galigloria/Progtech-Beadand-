07ecb0b64efb290cda52b3599443eacb8ee23e7d
farkastomi01 <122123045+farkastomi01@users.noreply.github.com>
2024-05-12 22:42:07 +0200

    Add files via upload

---
+++ Admin.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace LegoStoreManagementSystem
+{
+    partial class admin_Add_user
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.order = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Admin_close = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.adminAddProducts1 = new LegoStoreManagementSystem.AdminAddProducts();
+            this.Admin_Products_button = new System.Windows.Forms.Button();
+            this.Admin_Dash_Button = new System.Windows.Forms.Button();
+            this.Admin_Employee_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.employeeAddUser1 = new LegoStoreManagementSystem.EmployeeAddUser();
+            this.adminDash1 = new LegoStoreManagementSystem.AdminDash();
+            this.adminAddProducts2 = new LegoStoreManagementSystem.AdminAddProducts();
+            this.order1 = new LegoStoreManagementSystem.Order();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.order);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.Admin_close);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.Admin_Products_button);
+            this.panel1.Controls.Add(this.Admin_Dash_Button);
+            this.panel1.Controls.Add(this.Admin_Employee_button);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-17, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(317, 901);
+            this.panel1.TabIndex = 0;
+            // 
+            // order
+            // 
+            this.order.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.order.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.order.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(36)))), ((int)(((byte)(28)))));
+            this.order.Location = new System.Drawing.Point(90, 401);
+            this.order.Name = "order";
+            this.order.Size = new System.Drawing.Size(157, 60);
+            this.order.TabIndex = 9;
+            this.order.Text = "Rendelések ";
+            this.order.UseVisualStyleBackColor = false;
+            this.order.Click += new System.EventHandler(this.order_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(28)))), ((int)(((byte)(36)))));
+            this.label1.Location = new System.Drawing.Point(60, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Vásárlói felület";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(28)))), ((int)(((byte)(36)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(65, 739);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 69);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "KILÉPÉS";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Admin_close
+            // 
+            this.Admin_close.AutoSize = true;
+            this.Admin_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Admin_close.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(28)))), ((int)(((byte)(36)))));
+            this.Admin_close.Location = new System.Drawing.Point(17, 4);
+            this.Admin_close.Name = "Admin_close";
+            this.Admin_close.Size = new System.Drawing.Size(27, 25);
+            this.Admin_close.TabIndex = 5;
+            this.Admin_close.Text = "X";
+            this.Admin_close.Click += new System.EventHandler(this.Admin_close_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.adminAddProducts1);
+            this.panel2.Location = new System.Drawing.Point(323, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1290, 881);
+            this.panel2.TabIndex = 1;
+            // 
+            // adminAddProducts1
+            // 
+            this.adminAddProducts1.Location = new System.Drawing.Point(419, 190);
+            this.adminAddProducts1.Name = "adminAddProducts1";
+            this.adminAddProducts1.Size = new System.Drawing.Size(1042, 687);
+            this.adminAddProducts1.TabIndex = 0;
+            // 
+            // Admin_Products_button
+            // 
+            this.Admin_Products_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.Admin_Products_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Admin_Products_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(36)))), ((int)(((byte)(28)))));
+            this.Admin_Products_button.Location = new System.Drawing.Point(89, 335);
+            this.Admin_Products_button.Name = "Admin_Products_button";
+            this.Admin_Products_button.Size = new System.Drawing.Size(157, 60);
+            this.Admin_Products_button.TabIndex = 4;
+            this.Admin_Products_button.Text = "Termékek";
+            this.Admin_Products_button.UseVisualStyleBackColor = false;
+            this.Admin_Products_button.Click += new System.EventHandler(this.Admin_Products_button_Click);
+            // 
+            // Admin_Dash_Button
+            // 
+            this.Admin_Dash_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.Admin_Dash_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Admin_Dash_Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(36)))), ((int)(((byte)(28)))));
+            this.Admin_Dash_Button.Location = new System.Drawing.Point(89, 203);
+            this.Admin_Dash_Button.Name = "Admin_Dash_Button";
+            this.Admin_Dash_Button.Size = new System.Drawing.Size(157, 60);
+            this.Admin_Dash_Button.TabIndex = 2;
+            this.Admin_Dash_Button.Text = "Főoldal";
+            this.Admin_Dash_Button.UseVisualStyleBackColor = false;
+            this.Admin_Dash_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Admin_Employee_button
+            // 
+            this.Admin_Employee_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.Admin_Employee_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Admin_Employee_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(36)))), ((int)(((byte)(28)))));
+            this.Admin_Employee_button.Location = new System.Drawing.Point(89, 269);
+            this.Admin_Employee_button.Name = "Admin_Employee_button";
+            this.Admin_Employee_button.Size = new System.Drawing.Size(157, 60);
+            this.Admin_Employee_button.TabIndex = 1;
+            this.Admin_Employee_button.Text = "Dolgozók";
+            this.Admin_Employee_button.UseVisualStyleBackColor = false;
+            this.Admin_Employee_button.Click += new System.EventHandler(this.Admin_Employee_button_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LegoStoreManagementSystem.Properties.Resources.lego;
+            this.pictureBox1.Location = new System.Drawing.Point(65, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 111);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // employeeAddUser1
+            // 
+            this.employeeAddUser1.Location = new System.Drawing.Point(306, 15);
+            this.employeeAddUser1.Name = "employeeAddUser1";
+            this.employeeAddUser1.Size = new System.Drawing.Size(1063, 512);
+            this.employeeAddUser1.TabIndex = 8;
+            // 
+            // adminDash1
+            // 
+            this.adminDash1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.adminDash1.Location = new System.Drawing.Point(309, 15);
+            this.adminDash1.Name = "adminDash1";
+            this.adminDash1.Size = new System.Drawing.Size(790, 500);
+            this.adminDash1.TabIndex = 7;
+            // 
+            // adminAddProducts2
+            // 
+            this.adminAddProducts2.Location = new System.Drawing.Point(306, 3);
+            this.adminAddProducts2.Name = "adminAddProducts2";
+            this.adminAddProducts2.Size = new System.Drawing.Size(969, 512);
+            this.adminAddProducts2.TabIndex = 1;
+            // 
+            // order1
+            // 
+            this.order1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.order1.Location = new System.Drawing.Point(297, 0);
+            this.order1.Name = "order1";
+            this.order1.Size = new System.Drawing.Size(1000, 900);
+            this.order1.TabIndex = 9;
+            this.order1.Load += new System.EventHandler(this.order1_Load);
+            // 
+            // admin_Add_user
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(1600, 900);
+            this.Controls.Add(this.order1);
+            this.Controls.Add(this.employeeAddUser1);
+            this.Controls.Add(this.adminDash1);
+            this.Controls.Add(this.adminAddProducts2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "admin_Add_user";
+            this.Text = "Admin";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Admin_Products_button;
+        private System.Windows.Forms.Button Admin_Dash_Button;
+        private System.Windows.Forms.Button Admin_Employee_button;
+        private System.Windows.Forms.Panel panel2;
+        private AdminAddProducts adminAddProducts1;
+        private System.Windows.Forms.Label Admin_close;
+        private System.Windows.Forms.Button button2;
+        private AdminDash adminDash1;
+        private EmployeeAddUser employeeAddUser1;
+        private AdminAddProducts adminAddProducts2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button order;
+        private Order order1;
+    }
+}
